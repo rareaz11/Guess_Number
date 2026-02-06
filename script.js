c71aba081eaff3c2d45d3598bb81971e3e4d8f04
rareaz11 <antoniozelic5@gmail.com>
2026-02-06 10:27:35 +0100

    modal logic added for losing or wining option and counter

--- script.js
+++ script.js
@@ -2,6 +2,21 @@ let listOfSecretNumber = [Math.floor(Math.random() * 30) + 1];
 let messageSelector = document.querySelector("#message");
 const listOfSecretInputs = document.querySelector(".hidden-numbers");
 let inputs = listOfSecretInputs.querySelectorAll("input");
+let numberOfTry = document.querySelector(".numberOfTry");
+const modal = document.getElementById("gameModal");
+const closeModal = document.querySelector(".restart-modal");
+const modalWin = document.getElementById("gameModalWin");
+closeModalWin = document.querySelector(".restart-modalWin");
+
+closeModal.addEventListener("click", function () {
+  modal.classList.remove("active");
+  location.reload();
+});
+
+closeModalWin.addEventListener("click", function () {
+  modalWin.classList.remove("active");
+  location.reload();
+});
 
 console.log(listOfSecretNumber);
 //console.log(listOfSecretNumber.length);
@@ -13,39 +28,51 @@ while (listOfSecretNumber.length < 3) {
     listOfSecretNumber.push(numForLoop);
   }
 }
-
+let counter = 5;
 //console.log(listOfSecretNumber);
 
 function checkNumber() {
+  //modalWin.classList.add("active");
   let number = document.querySelector(".yourChoise").value;
-
-  if (parseInt(number) < 1 || parseInt(number) > 30) {
-    // console.log("veci je");
-    messageSelector.innerHTML = "Izaberite broj izmedu 1 i 30";
-    messageSelector.style.color = "red";
-  }
-  //console.log(number);
-  if (listOfSecretNumber.includes(parseInt(number))) {
-    console.log("tu sam");
-    if (inputs[0].value === "?") {
-      inputs[0].value = number;
-      messageSelector.innerHTML = "Pogodak";
-      messageSelector.style.color = "black";
-    } else if (inputs[0].value !== number && inputs[1].value === "?") {
-      messageSelector.innerHTML = "Pogodak";
-      messageSelector.style.color = "black";
-      inputs[1].value = number;
-    } else if (
-      parseInt(inputs[0].value) !== parseInt(number) &&
-      parseInt(inputs[1].value) !== parseInt(number) &&
-      inputs[2].value === "?"
-    ) {
-      inputs[2].value = number;
-      messageSelector.innerHTML = "CESTITAMO!  POBJEDILI STE!";
-      messageSelector.style.color = "green";
+  if (number !== "") {
+    if (counter <= 0) {
+      // console.log("gotovo");
+      modal.classList.add("active");
     } else {
-      messageSelector.innerHTML = "Ne mogu se koristiti 2 ista broja";
+      counter--;
+      console.log(counter);
+      numberOfTry.innerHTML = counter;
+    }
+
+    if (parseInt(number) < 1 || parseInt(number) > 30) {
+      // console.log("veci je");
+      messageSelector.innerHTML = "Izaberite broj izmedu 1 i 30";
       messageSelector.style.color = "red";
     }
+    //console.log(number);
+    if (listOfSecretNumber.includes(parseInt(number))) {
+      console.log("tu sam");
+      if (inputs[0].value === "?") {
+        inputs[0].value = number;
+        messageSelector.innerHTML = "Pogodak";
+        messageSelector.style.color = "black";
+      } else if (inputs[0].value !== number && inputs[1].value === "?") {
+        messageSelector.innerHTML = "Pogodak";
+        messageSelector.style.color = "black";
+        inputs[1].value = number;
+      } else if (
+        parseInt(inputs[0].value) !== parseInt(number) &&
+        parseInt(inputs[1].value) !== parseInt(number) &&
+        inputs[2].value === "?"
+      ) {
+        inputs[2].value = number;
+        modalWin.classList.add("active");
+      } else {
+        messageSelector.innerHTML = "Ne mogu se koristiti 2 ista broja";
+        messageSelector.style.color = "red";
+      }
+    }
+  } else {
+    messageSelector.innerHTML = "U polje treba biti unesen broj";
   }
 }
